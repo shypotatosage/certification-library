1db18d2fa4057b5eaa1fb707e995e74d98a06a36
Mimi <91705093+shypotatosage@users.noreply.github.com>
2024-11-24 01:34:22 +0700

    chore: add empty state

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,9 +9,16 @@
         </div>
 
         <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-            @foreach ($books as $book)
-                <x-book-card :book=$book />
-            @endforeach
+            @forelse ($books as $book)
+                <x-book-card :book="$book" />
+            @empty
+                <div class="col-span-1 sm:col-span-2 md:col-span-3 lg:col-span-4 text-center">
+                    <div class="p-6 border-2 border-gray-300 rounded-lg bg-gray-100 dark:bg-gray-800 dark:border-gray-700">
+                        <p class="text-lg font-semibold text-gray-700 dark:text-gray-300">No books found</p>
+                        <p class="text-gray-500 dark:text-gray-400 mt-2">Try changing your search criteria or select a different category.</p>
+                    </div>
+                </div>
+            @endforelse
         </div>
     </div>
 @endsection
